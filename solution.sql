a15e06c15f33d3e6edbba394e1246abe98850ae7
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-23 12:03:14 +0600

    insert data into students

--- solution.sql
+++ solution.sql
@@ -27,3 +27,71 @@ CREATE TABLE enrollment (
     student_id INT REFERENCES students (student_id),
     course_id INT REFERENCES courses (course_id)
 );
+
+-- insert data into students
+INSERT INTO
+    students (
+        student_id,
+        student_name,
+        age,
+        email,
+        frontend_mark,
+        backend_mark,
+        status
+    )
+VALUES (
+        1,
+        'Sameer',
+        21,
+        'sameer@example.com',
+        48,
+        60,
+        NULL
+    ),
+    (
+        2,
+        'Zoya',
+        23,
+        'zoya@example.com',
+        52,
+        58,
+        NULL
+    ),
+    (
+        3,
+        'Nabil',
+        22,
+        'nabil@example.com',
+        37,
+        46,
+        NULL
+    ),
+    (
+        4,
+        'Rafi',
+        24,
+        'rafi@example.com',
+        41,
+        40,
+        NULL
+    ),
+    (
+        5,
+        'Sophia',
+        22,
+        'sophia@example.com',
+        50,
+        52,
+        NULL
+    ),
+    (
+        6,
+        'Hasan',
+        23,
+        'hasan@gmail.com',
+        43,
+        39,
+        NULL
+    );
+
+-- sss
