5b126bf5abc284bc60f4fda73b8b29b7f88b6b9a
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-23 11:56:13 +0600

    created students and courses table

--- solution.sql
+++ solution.sql
@@ -1,2 +1,22 @@
--- Active: 1729130742383@@127.0.0.1@5432@postgres
-CREATE DATABASE university_db
+-- Active: 1729130742383@@127.0.0.1@5432@university_db
+
+-- crating database
+CREATE DATABASE university_db;
+
+-- crating students table
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    frontend_mark INT NOT NULL,
+    backend_mark INT NOT NULL,
+    status VARCHAR(100)
+);
+
+-- creating courses table
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    credits INT NOT NULL
+);
